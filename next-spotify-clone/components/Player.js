bf12da8d590293d040d966786f45ace9f622d319
Gurprem Bhullar <bhullargurprem@gmail.com>
2022-02-09 14:41:08 -0500

    almost complete

--- next-spotify-clone/components/Player.js
+++ next-spotify-clone/components/Player.js
@@ -1,10 +1,11 @@
-import { 
+import {
     HeartIcon,
     VolumeUpIcon as VolumeDownIcon,
-}   from "@heroicons/react/outline";
+    VolumeUpIcon,
+} from "@heroicons/react/outline";
 import {
     FastForwardIcon,
-    PauseIcon, 
+    PauseIcon,
     PlayIcon,
     ReplyIcon,
     RewindIcon,
@@ -22,18 +23,18 @@ import spotifyApi from "../lib/spotify";
 function Player() {
     const spotifyApi = useSpotify();
     const { data: session, status } = useSession();
-    const [currentTrackId, setCurrentIdTrack] = 
-    useRecoilState(currentTrackIdState);
+    const [currentTrackId, setCurrentIdTrack] =
+        useRecoilState(currentTrackIdState);
     const [isPlaying, setIsPlaying] = useRecoilState(isPlayingState);
     const [volume, setVolume] = useState(50);
-    
+
     const songInfo = useSongInfo();
     // this is a function to fetch the current song
 
-    const fetchCurrentSong = () => {  
+    const fetchCurrentSong = () => {
         if (!songInfo) {
             spotifyApi.getMyCurrentPlayingTrack().then(data => {
-                console.log("Now playing: ", data.body?.item);    
+                console.log("Now playing: ", data.body?.item);
                 setCurrentIdTrack(data.body?.item?.id);
 
                 spotifyApi.getMyCurrentPlaybackState().then((data) => {
@@ -42,25 +43,36 @@ function Player() {
             });
         }
     }
+    
+    const handlePlayPause = () => {
+        spotifyApi.getMyCurrentPlaybackState().then((data) => 
+        {
+            if (data.body.is_playing) {
+                spotifyApi.pause();
+                setIsPlaying(false);
+            } else
+            spotifyApi.play();
+            setIsPlaying(true);
+        }
+    )};
 
     useEffect(() => {
-        if (spotifyApi.getAccessToken() && !currentTrackId)
-    {
-        // fetch the song info
-        fetchCurrentSong();
-        setVolume[50];
-    }
-},[currentTrackIdState, spotifyApi, session])
-    
+        if (spotifyApi.getAccessToken() && !currentTrackId) {
+            // fetch the song info
+            fetchCurrentSong();
+            setVolume[50];
+        }
+    }, [currentTrackIdState, spotifyApi, session])
 
-return (                                                                   //text will be really small on the mobil screen 
+
+    return (                                                                   //text will be really small on the mobil screen 
         <div className="h-24 bg-gradient-to-b from-black to-gray-900 text-white grid grid-cols-3 text-xs md:text-base px-2 md:px-8">
             {/* Left */}
-            <div className="flex items-center space-x-4"> 
-                <img 
-                className="hidden md:inline h-10 w-10"      // as the screen gets bigger you will see this on 
-                src={songInfo?.album.images?.[0]?.url}      // the bottom left hand corner.
-                alt="" 
+            <div className="flex items-center space-x-4">
+                <img
+                    className="hidden md:inline h-10 w-10"     
+                    src={songInfo?.album.images?.[0]?.url}     
+                    alt=""
                 />
                 <div>
                     <h3>{songInfo?.name}</h3>
@@ -70,17 +82,23 @@ return (                                                                   //tex
 
             {/* Center */}
             <div className="flex items-center justify-evenly">
-                <SwitchHorizontalIcon className="button"  />
-                <RewindIcon className="button"  />
+                <SwitchHorizontalIcon className="button" />
+                <RewindIcon className="button" />
 
                 {isPlaying ? (
-                    <PauseIcon className="button w-10 h-10" />
-                )  : (
-                    <PlayIcon className="button w-10 h-10" />
+                    <PauseIcon onClick={handlePlayPause} className="button w-10 h-10" />
+                ) : (
+                    <PlayIcon onClick={handlePlayPause} className="button w-10 h-10" />
                 )}
 
                 <FastForwardIcon className="button" />
                 <ReplyIcon className="button" />
+            </div>
+         {/*Right */}
+            <div className="flex items-center space-x-3 md:space-x-4 justify-end pr-5">
+                <VolumeDownIcon className="buttton w-10 h-10" />
+                <input type="range" value="" min={0} max={100} />
+                <VolumeUpIcon className="button w-10 h-10"/>
             </div>
         </div>
     );
